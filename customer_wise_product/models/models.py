dc3809f645397ece04308dbb3654a25b8d319c27
Umairsarwar321 <69766582+Umairsarwar321@users.noreply.github.com>
2022-12-07 19:04:58 +0500

    Add files via upload
    Customer products

--- customer_wise_product/models/models.py
+++ customer_wise_product/models/models.py
@@ -29,30 +29,6 @@ class InheritProductTemplate(models.Model):
                     rec.customer_ids = vals.get('customer_ids')
         return res
 
-    def name_get(self):
-        result = []
-        if self.env.context.get('sale') or (self.env.context.get('move_type') == 'out_invoice'):
-            partner_id = self.env.context.get('partner_id')
-            products = self.env['product.template'].search([('customer_ids', '=', partner_id)])
-            if len(products.ids) > 0:
-                for product in products:
-                    name = '[%s] %s' % (
-                        product.default_code, product.name) if product.default_code else '%s' % product.name
-                    result.append((product.id, name))
-                return result
-            else:
-                return result
-        else:
-            return self.get_normal_result()
-
-    def get_normal_result(self):
-        result = []
-        for product in self:
-            name = '[%s] %s' % (
-                product.default_code, product.name) if product.default_code else '%s' % product.name
-            result.append((product.id, name))
-        return result
-
 
 class InheritProductProduct(models.Model):
     _inherit = 'product.product'
@@ -60,37 +36,82 @@ class InheritProductProduct(models.Model):
     customer_ids = fields.Many2many('res.partner', string="Customers")
 
     def name_get(self):
+        # TDE: this could be cleaned a bit I think
+        def _name_get(d):
+            name = d.get('name', '')
+            code = self._context.get('display_default_code', True) and d.get('default_code', False) or False
+            if code:
+                name = '[%s] %s' % (code, name)
+            return (d['id'], name)
+
+        partner_id = self._context.get('partner_id')
+        if partner_id:
+            partner_ids = [partner_id, self.env['res.partner'].browse(partner_id).commercial_partner_id.id]
+        else:
+            partner_ids = []
+        company_id = self.env.context.get('company_id')
+
+        # all user don't have access to seller and partner
+        # check access and use superuser
+        self.check_access_rights("read")
+        self.check_access_rule("read")
+
         result = []
+
+        # Prefetch the fields used by the `name_get`, so `browse` doesn't fetch other fields
+        # Use `load=False` to not call `name_get` for the `product_tmpl_id`
+        self.sudo().read(['name', 'default_code', 'product_tmpl_id'], load=False)
+
+        product_template_ids = self.sudo().mapped('product_tmpl_id').ids
+
+        if partner_ids:
+            supplier_info = self.env['product.supplierinfo'].sudo().search([
+                ('product_tmpl_id', 'in', product_template_ids),
+                ('name', 'in', partner_ids),
+            ])
+            # Prefetch the fields used by the `name_get`, so `browse` doesn't fetch other fields
+            # Use `load=False` to not call `name_get` for the `product_tmpl_id` and `product_id`
+            supplier_info.sudo().read(['product_tmpl_id', 'product_id', 'product_name', 'product_code'], load=False)
+            supplier_info_by_template = {}
+            for r in supplier_info:
+                supplier_info_by_template.setdefault(r.product_tmpl_id, []).append(r)
         if self.env.context.get('sale') or (self.env.context.get('move_type') == 'out_invoice'):
             partner_id = self.env.context.get('partner_id')
             products = self.env['product.product'].search([('customer_ids', '=', partner_id)])
-            if len(products.ids) > 0:
-                for product in products:
-                    var_label = self.get_var_label(product)
-                    name = '[%s] %s %s' % (
-                        product.default_code, product.name, var_label) if product.default_code else '%s %s' % (
-                        product.name, var_label) if var_label else '%s' % product.name
-                    result.append((product.id, name))
-                return result
-            else:
-                return result
         else:
-            return self.get_normal_result()
-
-    @staticmethod
-    def get_var_label(product=None):
-        labels = []
-        for rec in product:
-            for label in rec.product_template_variant_value_ids:
-                labels.append(label.name)
-        return tuple(labels)
-
-    def get_normal_result(self):
-        result = []
-        for product in self:
-            var_label = self.get_var_label(product)
-            name = '[%s] %s %s' % (
-                product.default_code, product.name, var_label) if product.default_code and var_label else '%s %s' % (
-                product.name, var_label) if var_label else '%s' % product.name
-            result.append((product.id, name))
+            products = self.sudo()
+        for product in products:
+            variant = product.product_template_attribute_value_ids._get_combination_name()
+            name = variant and "%s (%s)" % (product.name, variant) or product.name
+            sellers = self.env['product.supplierinfo'].sudo().browse(self.env.context.get('seller_id')) or []
+            if not sellers and partner_ids:
+                product_supplier_info = supplier_info_by_template.get(product.product_tmpl_id, [])
+                sellers = [x for x in product_supplier_info if x.product_id and x.product_id == product]
+                if not sellers:
+                    sellers = [x for x in product_supplier_info if not x.product_id]
+                # Filter out sellers based on the company. This is done afterwards for a better
+                # code readability. At this point, only a few sellers should remain, so it should
+                # not be a performance issue.
+                if company_id:
+                    sellers = [x for x in sellers if x.company_id.id in [company_id, False]]
+            if sellers:
+                for s in sellers:
+                    seller_variant = s.product_name and (
+                            variant and "%s (%s)" % (s.product_name, variant) or s.product_name
+                    ) or False
+                    mydict = {
+                        'id': product.id,
+                        'name': seller_variant or name,
+                        'default_code': s.product_code or product.default_code,
+                    }
+                    temp = _name_get(mydict)
+                    if temp not in result:
+                        result.append(temp)
+            else:
+                mydict = {
+                    'id': product.id,
+                    'name': name,
+                    'default_code': product.default_code,
+                }
+                result.append(_name_get(mydict))
         return result
